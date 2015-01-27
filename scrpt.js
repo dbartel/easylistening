5319c58f469037134a20a5c320e81f158fabb255
Daniel <dan.m.bartel@gmail.com>
2015-01-27 14:48:49 -0600

    update site

--- scrpt.js
+++ scrpt.js
@@ -13,105 +13,8 @@ easyListeningApp.controller("MainCtrl", ["$scope", "$location", function($scope,
         { name: "Archive", url: "templates/archive.htm" }
     ];
            
-    $scope.changeActive = function(name) {
-        $scope.activePage = _.find($scope.templates, function(t) {
-            return t.name == name;
-        });
-    };
-    
-    $scope.parseUrl = function() {
-        switch ($location.path()) {
-                case "/about":
-                    $scope.changeActive("About");
-                    break;
-                case "/home": 
-                    $scope.changeActive("Home");
-                    break;  
-                default:
-                    $scope.changeActive("Home");
-        }
-    }
-    $scope.parseUrl();    
-    
 
-}]);
-
-easyListeningApp.controller("HomeCtrl", ["$scope", function($scope) {
-    //Set playing status
-    var d = new Date();    
-    if (d.getDay() == 3 && d.getUTCHours() == 18) {
-        $scope.showStatus = "maybe live right now";
-    }
-    else {
-        $scope.showStatus = "not live right now";
-    }
     
-    $scope.lastShow = {
-        name: "Studying music to help you study",
-        src: "https://db.tt/vsW4gd9G"
-    };
-    
-        
-    
-            
-}]);
-
-easyListeningApp.controller("AboutCtrl", ["$scope", "$timeout", function($scope, $timeout) {
-    
-    $scope.personSelected = false;
-    
-    $scope.daniel = {
-        name: "Daniel",
-        information: "Daniel hasn't done much of anything.",
-        image: "img/IMG_0300.JPG",
-        social: [
-            {
-                name: "Twitter",
-                url: "https://twitter.com/helloimdanielhi"
-            },
-            {
-                name: "last.fm",
-                url: "http://www.last.fm/user/myusernamegone"
-            }
-        ]              
-    };
-    
-    $scope.davis = {
-        name: "Davis",
-        information: "Davis is a guy named Davis. He has a zine that has cool things in it.",
-        image: "img/1.png",
-        social: [
-            {
-                name: "davisland.info",
-                url: "http://davisland.info/"
-            },
-            {
-                name: "in the end pretty much everything is mostly water",
-                url: "http://intheendprettymucheverythingismostlywater.com/"
-            }
-        ]
-    };
-    
-    $scope.changePerson = function(name) {
-        $scope.personSelected = false;
-
-        $timeout(function() {
-            
-            if (name == "Daniel") {
-                $scope.activePerson = $scope.daniel;
-            }
-            else if (name == "Davis") {
-                $scope.activePerson = $scope.davis;
-            } 
-            $scope.personSelected = true;
-        },500);
-    };
-    
-    $scope.activePerson = $scope.daniel;
-    
-}]);
-    
-easyListeningApp.controller("ArchiveCtrl", ["$scope", "$location", function($scope, $location) {
     $scope.archives = [
         {
             pic:"http://i.imgur.com/3101tp3.jpg",
@@ -128,10 +31,10 @@ easyListeningApp.controller("ArchiveCtrl", ["$scope", "$location", function($sco
             title: "Studying music to help you study",
             audioSrc:"https://db.tt/vsW4gd9G"
         }
-    ];
-    
+    ];    
 
     
-    
+
 }]);
+
     
