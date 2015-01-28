7f562d3eb3b0e4b5b6ab96e85c767d6d333ee185
Daniel <dan.m.bartel@gmail.com>
2015-01-27 18:02:39 -0600

    add show

--- scrpt.js
+++ scrpt.js
@@ -17,20 +17,27 @@ easyListeningApp.controller("MainCtrl", ["$scope", "$location", function($scope,
     
     $scope.archives = [
         {
-            pic:"http://i.imgur.com/3101tp3.jpg",
-            title: "Loop the radio!",
-            audioSrc:"https://db.tt/gDAVXKEn"
+            pic:"http://i.imgur.com/Dc14jWG.jpg",
+            title: "We're back!",
+            audioSrc: "https://db.tt/KvnnPNma"
+        },
+        {
+            pic:"http://i.imgur.com/7mco7bD.jpg",
+            title: "Studying music to help you stu  dy",
+            audioSrc:"https://db.tt/vsW4gd9G"
         },
         {
             pic:"http://i.imgur.com/COLZFQP.jpg",
             title: "Davis and Daniel present: An Easy Listening Christmas",
             audioSrc:"https://db.tt/rofhlIuV"
         },
+        
         {
-            pic:"http://i.imgur.com/7mco7bD.jpg",
-            title: "Studying music to help you study",
-            audioSrc:"https://db.tt/vsW4gd9G"
+            pic:"http://i.imgur.com/3101tp3.jpg",
+            title: "Loop the radio!",
+            audioSrc:"https://db.tt/gDAVXKEn"
         }
+
     ];    
 
     
